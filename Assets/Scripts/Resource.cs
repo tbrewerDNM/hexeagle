20c8c5cfbbddc927914c819f191dd8f799ce8870
tbrewer <terence.brewer@student.nmt.edu>
2020-02-19 13:59:51 -0700

    town prototype

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resource : MonoBehaviour
+{
+    public int workers = 0;
+    public int wood = 0;
+    public Image progressBar;
+    public Text woodCount;
+    public Text workerCount;
+
+    private float baseWidth;
+    private Vector3 basePos;
+    private float percent = 1;
+
+    // Start is called before the first frame update
+    void Start() {
+        this.basePos = progressBar.rectTransform.localPosition;
+        this.baseWidth = progressBar.rectTransform.sizeDelta.x;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (percent <= 0) {
+            percent = 1;
+            wood++;
+            woodCount.text = "Wood: " + wood;
+        } else {
+            percent -= 0.01f * workers;
+        }
+        changeBar(percent);
+    }
+
+    public void increaseWorkers() {
+        this.workers++;
+        workerCount.text = "Woodcutters: " + workers;
+    }
+
+    public void showMenu(GameObject menu) {
+        menu.SetActive(true);
+    }
+
+    public void hideMenu(GameObject menu) {
+        menu.SetActive(false);
+    }
+
+    private void changeBar(float percent) {
+        float newWidth = baseWidth * percent;
+        progressBar.rectTransform.localPosition = new Vector3(basePos.x - (baseWidth - newWidth) / 2, basePos.y, basePos.z);
+        progressBar.rectTransform.sizeDelta = new Vector2(newWidth, progressBar.rectTransform.sizeDelta.y);
+    }
+}
---
+++ Assets/UnitMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitMenu : MonoBehaviour
+{
+
+    public RectTransform menu;
+    public Slider slider;
+
+    private float end;
+
+    // Start is called before the first frame update
+    void Start() {
+        // 140 is the height of each child
+        float y = menu.GetChild(0).GetComponent<RectTransform>().sizeDelta.y;
+        end = -(y / 2) + (menu.childCount - 3) * y;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        RectTransform rt = menu.GetComponent<RectTransform>();
+        rt.localPosition = new Vector3(rt.localPosition.x, slider.value * end, rt.localPosition.z);
+    }
+}
